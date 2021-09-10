261ae44ca226f0756f4dd3afc11930d01fcc6b85
tn-teamrg <85945974+tn-teamrg@users.noreply.github.com>
2021-09-10 13:20:41 -0400

    define API root for authors and books

---
+++ src/main/java/com/readlearncode/dukesbookshop/restserver/RESTConfig.java
@@ -0,0 +1,10 @@
+package com.readlearncode.dukesbookshop.restserver;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class RESTConfig extends Application{
+
+
+}
---
+++ src/main/java/com/readlearncode/dukesbookshop/restserver/rest/AuthorResource.java
@@ -0,0 +1,10 @@
+package com.readlearncode.dukesbookshop.restserver.rest;
+
+import javax.ejb.Stateless;
+import javax.ws.rs.Path;
+
+@Stateless
+@Path("/authors")
+public class AuthorResource {
+
+}
